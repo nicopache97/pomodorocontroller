d4ef58a9e5acc37eeb5571ce5e19f71e8482744e
nicopache97 <nicopacheco1997@gmail.com>
2023-03-30 22:15:51 -0300

    agregue barra de porcentaje, funciona el audio alarma, agrege boton de mute

--- app/controllers/pomodoro_controller.rb
+++ app/controllers/pomodoro_controller.rb
@@ -1,4 +1,23 @@
 class PomodoroController < ApplicationController
+  
   def index
+    @mute=params[:mute]
+    if (@mute==nil)
+    then
+      @mute=false;
+    end
+
+    #cookies[:user_id] = {
+    #  value: current_user.id,
+    #  expires: 1.day.from_now,
+    #  secure: Rails.env.production?
+    #}
+
+
   end
+
+    private
+    def pomodoro_params
+    	params.require(:pomodoro).permit(  :mute , :tiempo )
+    end
 end
